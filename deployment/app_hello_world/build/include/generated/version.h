e5728632ef97dc1ca048f17a1c82289a5c8bcd2f
Angel Canelo <acanelo@hanyang.ac.kr>
2022-11-01 11:00:38 +0900

    removing unnecessary

--- deployment/app_hello_world/build/include/generated/version.h
+++
@@ -1,2 +0,0 @@
-#define firmware_VERSION_CODE 65536
-#define firmware_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
--- deployment/classification/BUILD/GAP8_V2/GCC_RISCV_FREERTOS/gap_sdk/rtos/freeRTOS/freertos_kernel/portable/GCC/RI5CY-GAP8/port_asm.d
+++
@@ -1,3 +0,0 @@
-/module/examples/ai/classification/BUILD/GAP8_V2/GCC_RISCV_FREERTOS//gap_sdk/rtos/freeRTOS/freertos_kernel/portable/GCC/RI5CY-GAP8/port_asm.o: \
- /gap_sdk/rtos/freeRTOS/freertos_kernel/portable/GCC/RI5CY-GAP8/port_asm.S \
- /gap_sdk/rtos/freeRTOS/freertos_kernel/portable/GCC/RI5CY-GAP8/chip_specific_extensions/gap8/freertos_risc_v_chip_specific_extensions.h
